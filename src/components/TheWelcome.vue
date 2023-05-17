011c83ea12fa96066d33d36a5345a02b31d9cdac
David Aslan <david.aslan@iver.se>
2023-05-17 16:46:52 +0200

    Adding bootrsap and prepare project

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -12,7 +12,7 @@ import SupportIcon from './icons/IconSupport.vue'
     <template #icon>
       <DocumentationIcon />
     </template>
-    <template #heading>Documentation</template>
+    <template #heading>Now you can change this</template>
 
     Vue’s
     <a href="https://vuejs.org/" target="_blank" rel="noopener">official documentation</a>
