a9683d95598f8c6fb5e1a7e52faac176958a0327
David Aslan <david.aslan@iver.se>
2023-05-18 16:44:26 +0200

    Dashboard layout and projectfiles

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -9,11 +9,59 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <h1 class="yellow">{{ msg }}</h1>
     <h3>
       Mysig morgon och trevlig kväll 
     </h3>
+    
+    <h3>OBS! Ojämn fördelning</h3>
   </div>
+
+  <div class="container text-center ">
+    <div class="row">
+      <div class="col-4 tbl">
+        Tider
+      </div>
+      <div class="col-6 ">
+        Tekniker
+      </div>
+    </div>
+    <div class="row">
+      <div class="col tbl">
+        11:00-12:00
+      </div>
+      <div class="col">
+        David
+      </div>
+      <div class="col">
+        David
+      </div>
+      <div class="col">
+        David
+      </div>
+      <div class="col">
+        David
+      </div>
+    </div>
+    <div class="row">
+      <div class="col tbl">
+        12:00-13:00
+      </div>
+      <div class="col">
+        David
+      </div>
+      <div class="col">
+        David
+      </div>
+      <div class="col">
+        David
+      </div>
+    </div>
+  </div>
+
+    <div>
+
+    </div>
 </template>
 
 <style scoped>
@@ -27,6 +75,10 @@ h3 {
   font-size: 1.2rem;
 }
 
+.tbl {
+text-align: left;
+white-space: nowrap;
+}
 .greetings h1,
 .greetings h3 {
   text-align: center;
